286a17665e8f5ddc3b9b4f07d66bf9b0da9f0b79
Will Wadman <wadmanwill@gmail.com>
2022-10-17 19:08:21 +0000

    this is the assertEqual activity

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// Function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🏅🏅🏅🏅 Assertion Passed: ", actual + " === " + expected);
+  } else {
+    console.log("🥊🥊🥊🥊Assertion Failed: ", actual + " !== " + expected);
+  }
+};
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 3);
+assertEqual("Yes", "Yes");
