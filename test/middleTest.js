4a56c8421988a6821f52eb57c191b76546a94bfc
Will Wadman <wadmanwill@gmail.com>
2022-10-24 13:56:44 +0000

    Refactoring lotide using modules

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual')
+const middle = require('../middle')
+
+
+assertArraysEqual(middle([1,2,3]),[2]); // = > true
