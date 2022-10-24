7b00b6349f4fe38a3703882db14e1ef08ffdf0de
Will Wadman <wadmanwill@gmail.com>
2022-10-24 20:24:42 +0000

    Refactoring index of functions storted in an object

--- testing.js
+++ testing.js
@@ -1,2 +1,19 @@
+const hairColour = "red"
+const height = "tall"
+const person = {
+  name: "Dave",
+  eyes: 2,
+  hairColour,
+  height
+}
 
+const person2 = {
+  name: "Michael",
+  eyes: 2,
+  hairColour,
+  height: "Short"
+
+}
+console.log(person)
+console.log(person2)
 
