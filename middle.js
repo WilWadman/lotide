771dc71431d533b6dc7b3e02ba60e97d83a3d9fe
Will Wadman <wadmanwill@gmail.com>
2022-10-24 14:24:38 +0000

    Implementing mocha and chai

--- middle.js
+++ middle.js
@@ -1,27 +1,7 @@
-const eqArrays = function(arr1, arr2) {
-  
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`🏅🏅🏅🏅 Assertion Passed: ${array1} === ${array2}`);
-    
-  } else {
-    console.log(`🥊🥊🥊🥊Assertion Failed: ${array1} !== ${array2}`);
-  
-  
-   
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual')
+
 const middle = function(arr){
   if (arr.length < 3){
     return [];
@@ -37,11 +17,5 @@ if(arr.length %2 !== 0){
 return value;
 }
 };
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
 
-assertArraysEqual(middle([1,2,3]),[2]); // = > true
+module.exports = middle;
--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,25 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head')
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 3);
-assertEqual("Yes", "Yes");
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it ("returns 'Lighthouse' for 'Lighthouse Labs'", ()=>{
+    assert.strictEqual(head("Lighthouse Labs"), "Bootcamp");
+  });
+  it ("returns  1 for 1 ", () => {
+    assert.strictEqual(head('1'), '1');
+  });
+  it ("returns '5' got [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+});
+
+
+
+
